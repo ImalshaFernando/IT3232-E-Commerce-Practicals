6740535d57fd4c9a9e88a20e7eb2d570b0088825
Imalsha Fernando <mni.fernando326@gmail.com>
2025-03-23 18:08:32 +0530

    Creation of Student Details Class

---
+++ 2025.03.21/DetailsController.java
@@ -0,0 +1,20 @@
+package lk.ac.vau.fas.ict;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+public class DetailsController {
+	@GetMapping("/details")
+	public String Details() {
+		String name = "Name: Imalsha";
+		String age = "Age: 23";
+		String regNo = "Reg.No : 2020ICT106";
+		String course = "COurse:ICT";
+		
+		return name+"<br>" + age + "<br>" + regNo+ "<br>" + course;	
+		}
+
+}
